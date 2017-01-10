3a87cf15d400674bda638be9e2dc1e8f36caef35
Leo <liangchen.shu@gmail.com>
2017-01-10 22:13:35 +0800

    Update WriteDesFile.java

--- src/com/dp/structural/facade/WriteDesFile.java
+++ src/com/dp/structural/facade/WriteDesFile.java
@@ -1,7 +1,7 @@
 package com.dp.structural.facade;
 
 /**
- * Created by I337300 on 1/10/2017.
+ * Created by on 1/10/2017.
  */
 public class WriteDesFile {
     public void writeFile(String fileName,String encryptContent) {
